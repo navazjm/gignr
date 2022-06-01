752c3880c8c94044e45404f084b066ea59e8c982
tehmj <michaelnavs@gmail.com>
2022-06-01 18:44:43 -0500

    :bug: fixed bug related to not getting gitignore templates when not in project dir

--- utils/templates.go
+++ utils/templates.go
@@ -1,25 +1,30 @@
 package utils
 
 import (
-	"os"
+	"fmt"
+	"path"
 	"path/filepath"
+	"runtime"
 	"sort"
 	"strings"
 
 	"github.com/spf13/cobra"
 )
 
+// returns an array of all .gitignore templates found in path/to/gignr/templates/
 func GetTemplates() []string {
-	// TODO: change os.GetWD(), this only works if the user is inside project's root dir.
-	path, err := os.Getwd() // set path to location of gignr
+	_, packagePath, _, ok := runtime.Caller(0) // packagePath -> path/to/gignr/utils/
 
-	if err != nil {
-		cobra.CheckErr(err)
+	if !ok {
+		fmt.Println("Error finding project directory")
 	}
 
-	path += "/templates/*.gitignore"
+	templatesPath := path.Dir(packagePath)
 
-	files, err := filepath.Glob(path)
+	templatesPath = templatesPath[:len(templatesPath)-5] // remove utils from path -> path/to/gignr
+	templatesPath += "templates/*.gitignore"             // set path to templates/*.gitignore -> path/to/gignr/templates/*.gitignore
+
+	files, err := filepath.Glob(templatesPath) // get all .gitignore files in path/to/gignr/templates
 
 	if err != nil {
 		cobra.CheckErr(err)
@@ -28,10 +33,10 @@ func GetTemplates() []string {
 	for i, file := range files {
 		filename := filepath.Base(file)        // remove $HOME/gignr/templates/ -> template.gitignore
 		filename = filename[:len(filename)-10] // removes .gitignore from file name -> template
-		files[i] = strings.ToLower(filename)
+		files[i] = strings.ToLower(filename)   // converts to lowercase
 	}
 
-	sort.Strings(files)
+	sort.Strings(files) // alphabetically sort the templates
 
 	return files
 }
