d48b9a0933acf391104ca3518bfbbc3d7ab72c08
tehmj <michaelnavs@gmail.com>
2022-06-01 15:10:29 -0500

    :sparkles: sort template names, found bug with os.Getwd()

--- utils/templates.go
+++ utils/templates.go
@@ -3,12 +3,15 @@ package utils
 import (
 	"os"
 	"path/filepath"
+	"sort"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 func GetTemplates() []string {
-	path, err := os.Getwd()
+	// TODO: change os.GetWD(), this only works if the user is inside project's root dir.
+	path, err := os.Getwd() // set path to location of gignr
 
 	if err != nil {
 		cobra.CheckErr(err)
@@ -23,9 +26,12 @@ func GetTemplates() []string {
 	}
 
 	for i, file := range files {
-		filename := filepath.Base(file)
-		files[i] = filename[:len(filename)-10] // removes .gitignore from file name
+		filename := filepath.Base(file)        // remove $HOME/gignr/templates/ -> template.gitignore
+		filename = filename[:len(filename)-10] // removes .gitignore from file name -> template
+		files[i] = strings.ToLower(filename)
 	}
+
+	sort.Strings(files)
 
 	return files
 }
