28256fd59a0a5b6b891a2fe55c54274e48a35053
tehmj <michaelnavs@gmail.com>
2022-06-10 03:06:26 -0500

    :lipstick: change short help seperator icon

--- pkg/tui/tui.go
+++ pkg/tui/tui.go
@@ -33,6 +33,7 @@ func NewModel() model {
 	templateList := list.New(templates, templateItemDelegate, 0, 33)
 	templateList.Title = "Welcome to Gignr - Generate .gitignore templates at ease"
 	templateList.Styles.Title = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFFDF5"))
+  templateList.Help.ShortSeparator = "  "
 	templateList.Paginator.PerPage = 25
 	templateList.Paginator.SetTotalPages(len(gitignoreTemplates))
 
